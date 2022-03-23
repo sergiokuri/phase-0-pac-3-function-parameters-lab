131ebaf99640458f258f7eb1b4ff468c49d2c535
Sergio <kurisergio777777@gmail.com>
2022-03-22 19:02:24 -0600

    commit tree e0236f947a418d8d26577f739b7c64ee57713187

---
+++ index.js
@@ -0,0 +1,88 @@
+function introduction(name){
+    console.log(`Hi, my name is ${name}`)
+    return(`Hi, my name is ${name}`)
+
+
+}
+
+introduction ("Aki");
+introduction ("Samip")
+
+function introductionWithLanguage (name,language){
+    console.log (`Hi, my name is ${name} and I am learning to program in ${language}`)
+    return(`Hi,my name is ${name} and I am learning to program in ${language}`)
+}
+
+introductionWithLanguage("Aki","Ember.js")
+introductionWithLanguage("Samip","React")
+
+function introductionWithLanguageOptional (name,language = "JavaScript"){
+    console.log (`my name is  ${name3} and I am learning to program in ${language}`)
+    return (`my name is  ${name3} and I am learning to program in ${language}`)
+}
+introductionWithLanguageOptional ("Gracie");
+
+
+function introductionWithLanguageOptional (name,language = "JavaScript"){
+    console.log (`my name is  ${name} and I am learning to program in ${language}`)
+    return (`my name is  ${name} and I am learning to program in ${language}`)
+}
+
+introductionWithLanguageOptional("Gracie", "Python");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let name = "Aki";
+function introduction (name) {
+    console.log(`Hi, my name is ${name}`)
+    
+
+}
+
+let name0 = "Samip"
+function introduction (name2) {
+  console.log (console.log(`Hi, my name is ${name0}`))
+  
+}
+
+let name1 = "Aki";
+let language = "Ember.js";
+
+function introductionWithLanguage (name,language){
+    console.log (`my name is  ${name1} and I am learning to program in ${language}`)
+    let name2 = "Samip"
+    let language1 = "React";
+    console.log (`my name is  ${name2} and I am learning to program in ${language1}`)
+}
+
+
+
+let name3 = "Grazie";
+function introductionWithLanguageOptional (name,language = "JavaScript"){
+    console.log (`my name is  ${name3} and I am learning to program in ${language}`)
+
+}
+
+let name4 = "Grazie";
+function introductionWithLanguageOptional (name,language = "JavaScript"){
+    let language = "Python";
+    console.log (`my name is  ${name4} and I am learning to program in ${language}`)
